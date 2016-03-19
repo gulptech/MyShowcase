4fb50fe62e724ac2b7c97de6b6b0f8585d069334
Joseph Pilon <jpilon@gulptech.com>
2016-03-19 12:01:51 -0400

    Added fonts and 2 VC

---
+++ My Showcase/AuthorizedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AuthorizedViewController.swift
+//  My Showcase
+//
+//  Created by Joseph Pilon on 3/19/16.
+//  Copyright © 2016 Gulp Technologies. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
