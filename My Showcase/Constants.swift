72e9f44339688973d2a5eb1b90e34cd4ebee33ef
Joseph Pilon <jpilon@gulptech.com>
2016-03-19 15:19:30 -0400

    Login Interface done

---
+++ My Showcase/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  My Showcase
+//
+//  Created by Joseph Pilon on 3/19/16.
+//  Copyright © 2016 Gulp Technologies. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
